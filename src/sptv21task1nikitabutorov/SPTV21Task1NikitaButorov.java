fa6b21138ec5d4d7ccf05767ec6a3adfeb3c7d5e
NikitaButorov <butorov.nikita@list.ru>
2022-09-27 09:10:36 +0300

    first commit

---
+++ src/sptv21task1nikitabutorov/SPTV21Task1NikitaButorov.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptv21task1nikitabutorov;
+
+/**
+ *
+ * @author Nikita Butorov
+ */
+public class SPTV21Task1NikitaButorov {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Java Hello");
+        // TODO code application logic here
+    }
+    
+}
